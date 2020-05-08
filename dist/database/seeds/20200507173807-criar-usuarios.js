d05e2d48c062a6ace9b6535c4d19cf3a6da52d38
FrostPriest <johnericjahn@gmail.com>
2020-05-08 17:36:13 -0300

    Configuracao_Para_Deploy

---
+++ dist/database/seeds/20200507173807-criar-usuarios.js
@@ -0,0 +1,32 @@
+"use strict";const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'John',
+        email: 'johnericjahn@gmail.com',
+        password_hash: await bcrypt.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'John1',
+        email: 'johnericjahn1@gmail.com',
+        password_hash: await bcrypt.hash('654321', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'John2',
+        email: 'johnericjahn2@gmail.com',
+        password_hash: await bcrypt.hash('123456654321', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+  down: () => { },
+};
